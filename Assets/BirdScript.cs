e8ee8815eb8e221a1e32939ba3815f53adb1dbc4
ElifPinarBalbal <elifpinar999@gmail.com>
2025-06-06 20:32:53 +0300

    "Jump when Space is pressed" functionality added :)

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+
+    // Diðer component'larla iletiþim kurabilmek için onlarý reference etmemiz lazým!
+    public Rigidbody2D myRigidBody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Codes in Start will run as soon as the script is enabled!
+    // Runs only once
     void Start()
     {
         
     }
 
     // Update is called once per frame
+    // Update runs constantly while the script is enabled.
+
     void Update()
     {
-        
+        // Check whether the Space is pressed
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidBody.linearVelocity = Vector2.up * 10;
+        }
     }
 }
