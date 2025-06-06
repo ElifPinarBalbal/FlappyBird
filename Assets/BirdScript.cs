714be0cace4bfdc6033f4432ce9b88905cabeab9
ElifPinarBalbal <elifpinar999@gmail.com>
2025-06-07 01:49:11 +0300

    Game Over logic implemented (basic game features finished :))

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -8,12 +8,15 @@ public class BirdScript : MonoBehaviour
 
     public float flapStrength; // Unity'de böyle bir field create'lemiþ olduk!
 
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // Codes in Start will run as soon as the script is enabled!
     // Runs only once
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
@@ -22,9 +25,15 @@ public class BirdScript : MonoBehaviour
     void Update()
     {
         // Check whether the Space is pressed
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;   // Böylece ileride, farklý puanlar ekletebilirizz
         scoreText.text = playerScore.ToString();
+    }
 
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
